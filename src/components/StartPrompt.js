7a0449ca63c81ee853b6bda5f42678417034a652
Em <contactemilyhuang@gmail.com>
2022-05-20 10:31:49 -0700

    Adjust styling of game

--- src/components/StartPrompt.js
+++ src/components/StartPrompt.js
@@ -2,17 +2,19 @@ import '../styles/StartPrompt.css';
 
 const StartPrompt = () => {
   return (
-    <div id='start-prompt'>
-      <p>Find the items listed below as quickly as possible.</p>
-      <button>Start Game</button>
-      <p>Credits</p>
-      <p>
-        Photo by{' '}
-        <a href='https://www.flickr.com/photos/mag3737/' target='_blank'>
-          {' '}
-          Tom Magliery
-        </a>{' '}
-      </p>
+    <div class='start-cont center'>
+      <div id='start-prompt'>
+        <p>Find the items listed below as quickly as possible.</p>
+        <button>Start Game</button>
+        <p>Credits</p>
+        <p>
+          Photo by{' '}
+          <a href='https://www.flickr.com/photos/mag3737/' target='_blank'>
+            {' '}
+            Tom Magliery
+          </a>{' '}
+        </p>
+      </div>
     </div>
   );
 };
