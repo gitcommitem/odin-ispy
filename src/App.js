395b67e82963a7073263a107934da71c4d981ac4
Em <contactemilyhuang@gmail.com>
2022-05-23 18:55:44 -0700

    Add firebase and firestore config

--- src/App.js
+++ src/App.js
@@ -5,6 +5,30 @@ import BottomBar from './components/BottomBar';
 import Cursor from './components/Cursor';
 import React, { useState } from 'react';
 
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDUWg0z-qQLTn6DKLdBUsXJ93ie1orolao',
+
+  authDomain: 'ispygame-7ea84.firebaseapp.com',
+
+  projectId: 'ispygame-7ea84',
+
+  storageBucket: 'ispygame-7ea84.appspot.com',
+
+  messagingSenderId: '720457772463',
+
+  appId: '1:720457772463:web:a09f5c166f43aeedd4535a'
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
 function App () {
   const [isGameStart, setGameStart] = useState(false);
 
