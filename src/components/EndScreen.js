f4aecc4bc5ce3b6c1a217f3e37b94ef2b5aa1680
Em <contactemilyhuang@gmail.com>
2022-05-26 20:03:38 -0700

    Add function that displays top 5 times

--- src/components/EndScreen.js
+++ src/components/EndScreen.js
@@ -1,9 +1,29 @@
+import { collection } from 'firebase/firestore';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { db } from '../FirebaseConfig';
+
 const EndScreen = () => {
+  const [value, loading, error] = useCollection(collection(db, 'leaderboard'));
+  //Loading returns as true if still loading, false if data is retrieved
+
+  const top5 = [];
+
+  // Read firestore to list the leaderboard
+  (() => {
+    if (loading === false) {
+      value.docs.forEach(doc => {
+        let { score, time } = doc.data();
+        top5.push(<li key={doc.id}>{time}</li>);
+      });
+    }
+  })();
+
   return (
-    <div class="center">
+    <div className="center">
       <div>
         <h1>You found all items!</h1>
         <h1>Top 5</h1>
+        <ol>{top5}</ol>
       </div>
     </div>
   );
